83165483f2bba53d2b6cf5a6ef7ea8348810da7a
Adolfo Medina <a.star.for.me@hotmail.com>
2024-03-03 07:41:49 -0600

    Creating an empty ASP.NET Core project

---
+++ Chapter 13/Northwind.Web/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+// Start the web server, host the website, and wait for requests.
+app.Run(); // This is a thread-blocking call.
+WriteLine("This executes after the web server has stopped!");
