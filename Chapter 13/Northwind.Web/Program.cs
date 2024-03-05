45e2cb1bb977a0c7fba5ed133e5f69dafdee3e2f
Adolfo Medina <a.star.for.me@hotmail.com>
2024-03-04 23:17:41 -0600

    Enabling Razor Pages

--- Chapter 13/Northwind.Web/Program.cs
+++ Chapter 13/Northwind.Web/Program.cs
@@ -1,5 +1,12 @@
+#region Configure the web server host and services.
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+#endregion
 
 #region Configure the HTTP pipeline and routes
 
@@ -13,6 +20,7 @@ app.UseHttpsRedirection();
 app.UseDefaultFiles(); // index.html, default.html, and so on.
 app.UseStaticFiles();
 
+app.MapRazorPages();
 app.MapGet("/hello", () => $"Environment is {app.Environment.EnvironmentName}");
 
 #endregion
