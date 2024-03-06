7f9b9ff9fbe08284f727c242c4ab87c21a4d18de
Adolfo Medina <a.star.for.me@hotmail.com>
2024-03-05 20:57:24 -0600

    Enabling a model to insert entities

--- Chapter 13/Northwind.Web/Pages/Suppliers.cshtml.cs
+++ Chapter 13/Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc.RazorPages; // To use PageModel.
 using Northwind.EntityModels.Sqlite; // To use NorthwindContext.
+using Microsoft.AspNetCore.Mvc; // To use [BindProperty], IActionResult.
 
 namespace Northwind.Web.Pages;
 
 public class SuppliersModel : PageModel
 {
+	[BindProperty]
+	public Supplier? Supplier { get; set; }
 	public IEnumerable<Supplier>? Suppliers { get; set; }
 	private NorthwindContext _db;
 
@@ -20,5 +23,19 @@ public class SuppliersModel : PageModel
 		Suppliers = _db.Suppliers
 		.OrderBy(c => c.Country)
 		.ThenBy(c => c.CompanyName);
+	}
+
+	public IActionResult OnPost()
+	{
+		if (Supplier is not null && ModelState.IsValid)
+		{
+			_db.Suppliers.Add(Supplier);
+			_db.SaveChanges();
+			return RedirectToPage("/suppliers");
+		}
+		else
+		{
+			return Page(); // Return to original page.
+		}
 	}
 }
