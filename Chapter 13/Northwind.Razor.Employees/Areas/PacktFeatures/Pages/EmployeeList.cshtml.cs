d1a347db1a64f57febf33445deeeb3a929cf03bd
Adolfo Medina <a.star.for.me@hotmail.com>
2024-03-09 22:35:41 -0600

    Exercise 13.2 – Using Razor class libraries

---
+++ Chapter 13/Northwind.Razor.Employees/Areas/PacktFeatures/Pages/EmployeeList.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages; // To use PageModel.
+using Northwind.EntityModels.Sqlite; // To use Employee, NorthwindContext.
+
+namespace PacktFeatures.Pages;
+
+public class EmployeeListPageModel : PageModel
+{
+	private NorthwindContext _db;
+
+	public EmployeeListPageModel(NorthwindContext db)
+	{
+		_db = db;
+	}
+
+	public Employee[] Employees { get; set; } = null;
+
+	public void OnGet()
+	{
+		ViewData["Title"] = "Northwind B2B - Employees";
+
+		Employees = _db.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToArray();
+	}
+}
